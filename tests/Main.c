6025b23bf1564a4685b55be8385a243227d6882d
Christopher Bazley <cs99cjb@gmail.com>
2026-08-28 07:15:10 +0100

    Strengthen use of Fortify to check memory

--- tests/Main.c
+++ tests/Main.c
@@ -25,6 +25,23 @@
 /* Local headers */
 #include "Tests.h"
 
+#ifdef FORTIFY
+static int fortify_detected;
+
+static void fortify_check(void)
+{
+  Fortify_CheckAllMemory();
+  assert(!fortify_detected);
+}
+
+static void fortify_output(const char *text)
+{
+  fputs(text, stdout);
+  if (strstr(text, "detected"))
+    fortify_detected = 1;
+}
+#endif
+
 int main(int argc, char *argv[])
 {
   static const struct
@@ -41,6 +58,11 @@ int main(int argc, char *argv[])
   NOT_USED(argv);
 
   DEBUG_SET_OUTPUT(DebugOutput_StdOut, "");
+
+#ifdef FORTIFY
+  Fortify_SetOutputFunc(fortify_output);
+  atexit(fortify_check);
+#endif
 
   for (size_t count = 0; count < ARRAY_SIZE(test_groups); count++)
   {
